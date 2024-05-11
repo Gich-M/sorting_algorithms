5ee48acaffac22baecb07102c177e1dce66a1f70
Gich-M <gichmuriuki21@gmail.com>
2024-05-11 07:16:59 -0700

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,31 +15,33 @@ void swap_ints(int *a, int *b)
 }
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order.
+ * bubble_sort - Sorts an array of integers in ascending order using the
+ *               bubble sort algorithm.
  * @array: An array of integers to sort.
- * @size: The size of the array
+ * @size: The size of the array.
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+	size_t i, len;
+	bool bubbly = true;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	len = size;
+	while (bubbly)
 	{
-		bubbly = true;
-		for (i = 0; i < len; i++)
+		bubbly = false;
+		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + i, array + i + 1);
+				swap_ints(&array[i], &array[i + 1]);
 				print_array(array, size);
-				bubbly = false;
+				bubbly = true;
 			}
 		}
 		len--;
 	}
 }
+
